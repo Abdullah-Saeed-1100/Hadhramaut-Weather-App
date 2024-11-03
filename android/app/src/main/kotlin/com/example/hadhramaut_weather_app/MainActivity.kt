31ee124a8bf854df0f3d195d742359c94e63236c
abdullah bagar <abdullah.saeed.bagar@gmail.com>
2024-11-03 11:31:52 +0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/hadhramaut_weather_app/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.hadhramaut_weather_app
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/Services/services.dart
@@ -0,0 +1,27 @@
+import 'dart:convert';
+import 'package:http/http.dart' as http;
+
+import '../Model/weather_model.dart';
+
+class WeatherServices {
+  fetchWeather() async {
+    // Mukalla city
+    final response = await http.get(
+      Uri.parse(
+          "https://api.openweathermap.org/data/2.5/weather?lat=14.54248&lon=49.12424&appid=54eeeb7bb4500782bf24c70fa134e498"),
+    );
+    // now we can cange latitude and longitude and let's see how it perfrom.
+    try {
+      if (response.statusCode == 200) {
+        var json = jsonDecode(response.body);
+        return WeatherData.fromJson(json);
+      } else {
+        throw Exception('Failed to load Weather data');
+      }
+    } catch (e) {
+      print(e.toString());
+    }
+  }
+}
+// replace the api key with your api key thay openWeathemap provide you
+// for your current location provide the longitude and latitude of your current location
